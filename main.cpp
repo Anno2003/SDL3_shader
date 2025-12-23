0905b2f1b8581eb6eb861ec32b5a1585cf47bd47
anno2003 <idsale2002@yahoo.com>
2025-12-23 22:56:25 +0700

    cleanup; program now can read shader from vertex_shader.glsl and blur_shader.glsl

--- main.cpp
+++ main.cpp
@@ -5,11 +5,12 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <math.h>
 
 static SDL_Window *window = NULL;
-static SDL_Renderer *renderer = NULL;
 static SDL_GLContext gl_context = NULL;
 static GLuint shaderProgram;
+static GLuint VAO, VBO;
 
 void CheckShaderCompilation(GLuint shader, const char* type) {
     GLint success;
@@ -80,8 +81,9 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]){
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 	
     /* Create the window */
-    if (!SDL_CreateWindowAndRenderer("Glass Shader", 600, 600, SDL_WINDOW_TRANSPARENT|SDL_WINDOW_ALWAYS_ON_TOP|SDL_WINDOW_RESIZABLE|SDL_WINDOW_OPENGL, &window, &renderer)) {
-        SDL_Log("Couldn't create window and renderer: %s", SDL_GetError());
+	window = SDL_CreateWindow("Glass Shader", 600, 600, SDL_WINDOW_TRANSPARENT|SDL_WINDOW_ALWAYS_ON_TOP|SDL_WINDOW_RESIZABLE|SDL_WINDOW_OPENGL); 
+    if (!window) {
+        SDL_Log("Couldn't create window : %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
 	
@@ -91,6 +93,7 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]){
 		SDL_Log("Couldn't create gl context: %s", SDL_GetError());
         return SDL_APP_FAILURE;
 	}
+	SDL_GL_MakeCurrent(window, gl_context);
 	
 	glewExperimental = GL_TRUE; // Fixes issues with some drivers
     GLenum err = glewInit();
@@ -101,8 +104,35 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]){
         return SDL_APP_FAILURE;
     }
 	
-	shaderProgram = LoadShader("vertex_shader.glsl", "blur_shader.glsl");
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	
+	shaderProgram = LoadShader("vertex_shader.glsl", "frag_shader.glsl");
+	
+	float vertices[] = {
+		// Pos      // UV (Texture Coords)
+		-1.0f,  1.0f,  0.0f, 1.0f, // Top-Left
+		1.0f,  1.0f,  1.0f, 1.0f, // Top-Right
+		1.0f, -1.0f,  1.0f, 0.0f, // Bottom-Right
+
+		-1.0f,  1.0f,  0.0f, 1.0f, // Top-Left
+		1.0f, -1.0f,  1.0f, 0.0f, // Bottom-Right
+		-1.0f, -1.0f,  0.0f, 0.0f  // Bottom-Left
+	};
+
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
     return SDL_APP_CONTINUE;
 }
 
@@ -117,39 +147,28 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event){
 
 /* loop */
 SDL_AppResult SDL_AppIterate(void *appstate){
-    
-	const char *message = "Hello World!";
-    int w = 0, h = 0;
-    float x, y;
-    const float scale = 4.0f;
-
-    /* Center the message and scale it up */
-    SDL_GetRenderOutputSize(renderer, &w, &h);
-    SDL_SetRenderScale(renderer, scale, scale);
-    x = ((w / scale) - SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE * SDL_strlen(message)) / 2;
-    y = ((h / scale) - SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE) / 2;
-
-    /* Draw the message */
-    /*
-	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
-    SDL_RenderClear(renderer);
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_RenderDebugText(renderer, x, y, message);
-    SDL_RenderPresent(renderer);
-	*/
+	int w, h;
+	double timeValue = (double)SDL_GetTicks() / 1000.0;;
+	float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+	int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+	SDL_GetWindowSize(window, &w, &h);
+	glViewport(0, 0, w, h);
+	
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
         
 	glUseProgram(shaderProgram);
 	CheckGLError("glUseProgram");
 	
-	glUniform1i(glGetUniformLocation(shaderProgram, "screenTexture"), 0);
-	CheckGLError("glUniform1i");
-
-	glUniform2f(glGetUniformLocation(shaderProgram, "resolution"), 800.0f, 600.0f);
+	glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+	CheckGLError("glUniform4f");
+	
+	glUniform2f(glGetUniformLocation(shaderProgram, "resolution"), (float)w, (float)h);
 	CheckGLError("glUniform2f");
 
-	glUniform1f(glGetUniformLocation(shaderProgram, "blurRadius"), 2.0f);
-	CheckGLError("glUniform1f");
+	
+	glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
 	
 	SDL_GL_SwapWindow(window);
 	
@@ -158,6 +177,9 @@ SDL_AppResult SDL_AppIterate(void *appstate){
 
 /* This function runs once at shutdown. */
 void SDL_AppQuit(void *appstate, SDL_AppResult result){
-	SDL_GL_DestroyContext(gl_context);
-	SDL_DestroyWindow(window);
+	glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
+    SDL_GL_DestroyContext(gl_context);
+    SDL_DestroyWindow(window);
 }
